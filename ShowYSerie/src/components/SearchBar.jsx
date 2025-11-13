c468e2696a12293c5d413bd7d33ee1e78b506de9
XLLAMAA <jaimeadanez11@gmail.com>
2025-11-13 19:33:14 +0100

    Fix: He agregado cambios y mejorar pero no muchas

--- ShowYSerie/src/components/SearchBar.jsx
+++ ShowYSerie/src/components/SearchBar.jsx
@@ -1,25 +1,27 @@
 export default function SearchBar({value, onChange, onSubmit, loading}) {
 
-    const handleSubmit = (e) => {
-        //evita recarga de pagina
-        e.preventDefault();
-         //Si esta vacia la barra de busqueda no busca nada
-        if (!value || !value.trim()) { 
-            return;
-        }
-        //Aviso para que se haga la busquedas
-        onSubmit(); 
-    }
+  const handleSubmit = (e) => {
+      //evita recarga de pagina
+      e.preventDefault();
+      //Si esta vacia la barra de busqueda no busca nada
+      if (!value || !value.trim()) { 
+          return;
+      }
+      //Aviso para que se haga la busquedas
+      onSubmit(); 
+  }
 
-    return (<form onSubmit={handleSubmit}>
-        <input type="text" 
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
         placeholder="Buscar una serie"
         //texto que muestra el input
         value={value}
         //Cada tecla avisa al padre (app) para actualizar el valor (value)
         onChange={(e) => onChange(e.target.value)}  
-        />
-        <button type="submit">🔍</button>
-    </form>);
-
+      />
+      <button type="submit" disabled={loading}>🔍</button>
+    </form>
+  );
 }
--- ShowYSerie/src/components/ShowSerie.jsx
+++ ShowYSerie/src/components/ShowSerie.jsx
@@ -5,23 +5,23 @@ const getRating = (show) => show?.rating?.average ?? "—";
 
 export default function ShowSerie({show, onOpen, onToggleFav, fav}) {
    const title = getTitle(show);
-    const img = getImg(show);
-    const lang = getLang(show);
-    const rating = getRating(show);
-     //Muestro la imagen
-      //El titulo
-       //El lenguaje
-       //la puntuacion de la serie
-    return (
-        <div>
-            <img src={img} alt={title}/>    
-            <h2>{title}</h2>    
-            <p>{lang}</p>   
-            <p>{rating}</p>    
-            <div>
-                <button onClick={onOpen}>Detalles</button>
-                <button onClick={onToggleFav}>{fav ? "★ Quitar" : "☆ Favoritos"}</button>
-            </div>
+   const img = getImg(show);
+   const lang = getLang(show);
+   const rating = getRating(show);
+   //Muestro la imagen
+   //El titulo
+   //El lenguaje
+   //la puntuacion de la serie
+   return (
+      <div>
+        <img className="ImgSerie" src={img} alt={title}/>    
+        <h2 className="TituloSerie">{title}</h2>    
+        <p className="LengSerie">{lang}</p>   
+        <p className="RankingSerie">{rating}</p>    
+        <div className="BotonesSerie">
+          <button onClick={onOpen}>Detalles</button>
+          <button onClick={onToggleFav}>{fav ? "★ Quitar" : "☆ Favoritos"}</button>
         </div>
-    );
+      </div>
+   );
 }
